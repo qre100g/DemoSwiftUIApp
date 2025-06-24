42d6f08a651484c0f59373270e7c06266a08922c
qre100g <kmnrddy4334@gmail.com>
2025-06-25 00:34:51 +0530

    Initial Commit

---
+++ DemoApp/DemoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  DemoAppApp.swift
+//  DemoApp
+//
+//  Created by Mukesh Nagi Reddy on 25/06/25.
+//
+
+import SwiftUI
+
+@main
+struct DemoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
